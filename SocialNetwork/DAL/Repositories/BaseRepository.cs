9ec83ee0347d44525e3d6e4ca0a20f50ae404a14
Анна Башкатова <annabashk13@gmail.com>
2023-02-19 11:09:56 +0300

    Исправление ошибки в бд

--- SocialNetwork/DAL/Repositories/BaseRepository.cs
+++ SocialNetwork/DAL/Repositories/BaseRepository.cs
@@ -40,7 +40,7 @@ namespace SocialNetwork.DAL.Repositories
 
         private IDbConnection CreateConnection()
         {
-            return new SQLiteConnection("Data Source = DAL/DB/social_network_bd.db; Version = 3");
+            return new SQLiteConnection("Data Source = C:\\Users\\annab\\Desktop\\social_network_bd.db; Version = 3");
         }
     }
 }
